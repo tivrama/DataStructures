f0ebd2a383ff2778b5b343d8c1f9b74ae54d0185
tivrama <tivrama@yahoo.com>
2016-11-21 11:51:48 -0800

    Binary Tree methods

--- Collections/BinaryTree/BinaryTree.js
+++ Collections/BinaryTree/BinaryTree.js
@@ -8,7 +8,32 @@ class BinaryTree {
     this.right = null;
   }
 
+  addChild(val) {
+  	let side ='';
+  	this.value < val ? side = 'right' : side = 'left';
+
+		if (!this[side]) {
+			return this[side] = new BinaryTree(val);
+		} else {
+			return this[side].addChild(val)
+		}
+	}
+
+
+	contains(val) {
+		if (this.value === val) {
+			return true;
+		}
+		let side ='';
+  	this.value < val ? side = 'right' : side = 'left';
+		if (!this[side]) {
+			return false;
+		} else {
+			return this[side].contains(val);
+		}
+	}
 
 };
+
 
 module.exports = { BinaryTree };
--- Test/BinaryTreeTest.js
+++ Test/BinaryTreeTest.js
@@ -3,13 +3,32 @@
 const BinaryTree = require('../Collections/BinaryTree/BinaryTree.js').BinaryTree,
       assert = require('chai').assert;
 
-xdescribe('BinaryTree', () => {
+describe('BinaryTree', () => {
 
   describe('BinaryTree Create', () => {  
 
     it('creating new BinaryTree should take a value', () => {
       const myBinaryTree = new BinaryTree(1);
       assert.equal(myBinaryTree.value, 1, 'creat new BinaryTree not working')
+    });
+
+    let myBinaryTree = new BinaryTree(10);
+    myBinaryTree.addChild(5)
+    myBinaryTree.addChild(2)
+    myBinaryTree.addChild(7)
+    myBinaryTree.addChild(15)
+    myBinaryTree.addChild(17)
+    myBinaryTree.addChild(12)
+    myBinaryTree.addChild(11)
+
+    it('addChild should add grandchildren', () => {
+      assert.equal(myBinaryTree.right.left.left.value, 11, 'creat new BinaryTree not working')
+    });
+
+
+    it('contains should return true if value exists and false if not', () => {
+      assert.isTrue(myBinaryTree.contains(11));
+      assert.isFalse(myBinaryTree.contains(13));
     });
 
   });
