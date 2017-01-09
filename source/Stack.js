16256ce4a1cc6e52993bbe061b982541ff1bd210
tivrama <tivrama@yahoo.com>
2017-01-09 15:40:33 -0800

    Begin stack

--- source/Stack.js
+++ source/Stack.js
@@ -3,8 +3,33 @@
 class Stack {
 
   constructor(val) {
-    this.value = val;
-    this.stack = [];
+    if (val && Array.isArray(val)) {
+      this.stack = val;
+    } else if (val !== undefined) {
+      this.stack = [val];
+    } else {
+      this.stack = [];
+    }
+  }
+
+
+//-- CREATE -----------------------------
+
+  push(value) {
+    return this.stack.push(value);
+  }
+
+//-- DELETE -----------------------------
+
+  pop() {
+    return this.stack.pop();
+  }
+
+
+//-- HELPER -----------------------------
+
+  length() {
+    return this.stack.length;
   }
 
 }
