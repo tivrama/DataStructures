dd36c1d981f82028aa904246633a20b3856cf8be
tivrama <tivrama@yahoo.com>
2017-02-16 12:02:20 -0800

    Remove more recursion on Binary Tree - only delete left

--- source/BinaryTree.js
+++ source/BinaryTree.js
@@ -31,10 +31,11 @@ class BinaryTree {
 
 //-- CREATE -----------------------------
   addChild(val, id = null) {
+    if (val === undefined) return null;
     if (!id) {
       var id = this.makeId(false);
     }
-    var queue = [this];
+    let queue = [this];
     while (queue.length) {
       let node = queue.pop()
       let side = '';
@@ -49,29 +50,23 @@ class BinaryTree {
 
 //-- LOOKUP -----------------------------
   containsVal(val) {
+    if (val === undefined) return null;
     var queue = [this];
     while (queue.length) {
       let node = queue.pop();
-      if (node.value === val) {
-        return true;
-      }
-      if (node.left) {
-        queue.unshift(node.left);
-      }
-      if (node.right) {
-        queue.unshift(node.right);
-      }
+      if (node.value === val) return true;
+      if (node.left) queue.unshift(node.left);
+      if (node.right) queue.unshift(node.right);
     }
     return false;
   }
 
   containsId(id) {
+    if (!id) return null;
     var queue = [this];
     while (queue.length) {
       let node = queue.pop()
-      if (node.__id === id) {
-        return true;
-      }
+      if (node.__id === id) return true;
       let side = '';
       node.__id < id ? side = 'right' : side = 'left';
       if (!node[side]) {
@@ -86,26 +81,19 @@ class BinaryTree {
     var queue = [this];
     while (queue.length) {
       let node = queue.pop()
-      if (node.value === val) {
-        return node.__id;
-      }
-      if (node.left) {
-        queue.unshift(node.left);
-      }
-      if (node.right) {
-        queue.unshift(node.right);
-      }
+      if (node.value === val) return node.__id;
+      if (node.left) queue.unshift(node.left);
+      if (node.right) queue.unshift(node.right);
     }
     return null;
   }
 
   lookupId(id) {
+    if (!id) return null;
     var queue = [this];
     while (queue.length) {
       let node = queue.pop()
-      if (node.__id === id) {
-        return node.value;
-      }
+      if (node.__id === id) return node.value;
       let side = '';
       node.__id < id ? side = 'right' : side = 'left';
       if (!node[side]) {
@@ -118,12 +106,11 @@ class BinaryTree {
 
 //-- UPDATE -----------------------------
   updateId(id, val) {
+    if (!id || val === undefined) return null;
     var queue = [this];
     while (queue.length) {
       let node = queue.pop();
-      if (node.__id === id) {
-        return node.value = val;
-      }
+      if (node.__id === id) return node.value = val;
       let side ='';
       node.__id < id ? side = 'right' : side = 'left';
       if (node[side]) {
@@ -136,8 +123,8 @@ class BinaryTree {
 
 //-- DELETE -----------------------------
   deleteNode(id, parent = null, side = null, root = null) {
+    if (!id) return null;
     let idValue;
-    if (!id) null;
 
     if (!this.left && !this.right && parent === null) {
       if (this.__id !== id) {
@@ -161,7 +148,7 @@ class BinaryTree {
       if (node.right) saveChildren(node.right)
     }
     const addChildren = (originalRoot) => {
-      for (let i =0; i < children.length; i++) {
+      for (let i = 0; i < children.length; i++) {
         originalRoot.addChild(children[i].value, children[i].__id)
       }
     }
@@ -200,71 +187,73 @@ class BinaryTree {
   removeDuplicates() {
     let cache = {};
     let returnInfo = {};
-    const findAndRemoveDuplicates = (node, root = null) => {
-      if (!root) root = node;
+    let queue = [this];
+    while (queue.length) {
+      let node = queue.pop();
       if (!cache[node.value]) {
         cache[node.value] = node.__id;
       } else {
-        returnInfo[node.__id] = node.value
-        root.deleteNode(node.__id)
+        returnInfo[node.__id] = node.value;
+        this.deleteNode(node.__id);
       }
-      if (!node.left && !node.right) return
-      if (node.left) findAndRemoveDuplicates(node.left, root)
-      if (node.right) findAndRemoveDuplicates(node.right, root)
+      if (node.left) queue.unshift(node.left);
+      if (node.right) queue.unshift(node.right);
     }
-    findAndRemoveDuplicates(this)
     return returnInfo;
   }
 
 //-- HELPER -----------------------------
   mapToArray(cb) {
-    var resultArray = [];
-    const sub = (node) => {
-      if (node.left) { sub(node.left) }
+    let resultArray = [];
+    let queue = [this];
+    while (queue.length) {
+      let node = queue.pop();
+      if (node.left) queue.unshift(node.left);
       if (cb) {
         resultArray.push(cb(node.value));
       } else {
         resultArray.push(node.value);
       }
-      if (node.right) { sub(node.right) }
+      if (node.right) queue.unshift(node.right);
     }
-    sub(this);
     return resultArray;
   }
 
   mapIdToArray() {
-    var resultArray = [];
-    const sub = (node) => {
-      if (node.left) { sub(node.left) }
+    let resultArray = [];
+    let queue = [this];
+    while (queue.length) {
+      let node = queue.pop();
+      if (node.left) queue.unshift(node.left);
       resultArray.push(node.__id);
-      if (node.right) { sub(node.right) }
+      if (node.right) queue.unshift(node.right);
     }
-    sub(this);
     return resultArray;
   }
 
   filterToArray(cb) {
-    var resultArray = [];
-    const sub = (node) => {
-      if (node.left) { sub(node.left) }
-      if (cb(node.value)) {
-        resultArray.push(node.value);
-      }
-      if (node.right) { sub(node.right) }
+    if (!cb) return null;
+    let resultArray = [];
+    let queue = [this];
+    while (queue.length) {
+      let node = queue.pop();
+      if (node.left) queue.unshift(node.left);
+      if (cb(node.value)) resultArray.push(node.value);
+      if (node.right) queue.unshift(node.right);
     }
-    sub(this);
     return resultArray;
-  }
+  }  
 
 //-- DIGNOSTIC -----------------------------
   countNodes() {
     var count = 0;
-    const sub = (node) => {
-      if (node.left) { sub(node.left) }
-      count++;
-      if (node.right) { sub(node.right) }
+    var stack = [this];
+    while (stack.length) {
+      let node = stack.pop();
+      if (node.left) stack.push(node.left);
+      count++
+      if (node.right) stack.push(node.right)
     }
-    sub(this);
     return count;		
   }
 
